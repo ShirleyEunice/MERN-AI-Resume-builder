a02039ee4f0f49b303a36166ca6ca28f066ef78e
Shirley Eunice M <shirleyjustin20@gmail.com>
2025-12-09 13:20:57 +0530

    File structure created

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,16 @@
+
+
+const Banner = () => {
+  return (
+    <div className="w-full py-2.5 font-medium text-sm text-purple-800 text-center bg-linear-to-r from-[#d67eff] to-[#FDFEFF]">
+      <p>
+        <span className="px-3 py-1 rounded-lg text-white bg-purple-600 mr-2">
+          New
+        </span>
+        AI Feature Added
+      </p>
+    </div>
+  );
+}
+
+export default Banner
---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResumeBuilder = () => {
+  return (
+    <div>
+      <h1>Resume builder page</h1>
+    </div>
+  )
+}
+
+export default ResumeBuilder

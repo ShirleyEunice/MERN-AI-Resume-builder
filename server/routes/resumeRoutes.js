8a240a98ffe2fa576e3294acd93b18b44f122cd6
Shirley Eunice M <shirleyjustin20@gmail.com>
2026-01-18 00:42:43 +0530

    Backend integration completed overall - User model, Resume model, Ai model

---
+++ server/routes/resumeRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createResume, deleteResume, getResumeById, getResumeByIdPublic, updateResume } from "../controllers/resumeController.js";
+import upload from "../configs/multer.js";
+import protect from "../middlewares/authMiddleware.js";
+
+const resumeRouter = express.Router();
+
+resumeRouter.post('/create',protect, createResume);
+resumeRouter.post('/delete/:resumeId',protect, deleteResume);
+resumeRouter.put('/update', upload.single('image'), protect, updateResume);
+resumeRouter.get('/get/:resumeId',protect, getResumeById);
+resumeRouter.get('/public/:resumeId', getResumeByIdPublic);
+
+export default resumeRouter;

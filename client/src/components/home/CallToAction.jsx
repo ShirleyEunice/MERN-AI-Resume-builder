e63288b54fe4826f91e3b863b4aed5f7b7b913c3
Shirley Eunice M <shirleyjustin20@gmail.com>
2026-02-11 18:31:02 +0530

    Home screen updated to latest

--- client/src/components/home/CallToAction.jsx
+++ client/src/components/home/CallToAction.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 
 const CallToAction = () => {
@@ -6,7 +7,7 @@ const CallToAction = () => {
             <div className="flex flex-col md:flex-row text-center md:text-left items-center justify-between gap-8 px-3 md:px-10 border-x border-dashed border-slate-200 py-20 -mt-10 -mb-10 w-full">
                 <p className="text-xl font-medium max-w-sm">Build a Professional Resume That Helps You Stand Out and Get Hired</p>
                 <button className="flex items-center gap-2 rounded-md py-3 px-5 bg-purple-600 hover:bg-purple-700 transition text-white">
-                    <span>Get Started</span>
+                    <Link to="/app?state=register"><span>Get Started</span></Link>
                 </button>
             </div>
         </div>
